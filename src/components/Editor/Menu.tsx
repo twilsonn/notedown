1f4a6b93db55373728df1ea6df291d3a2cdd1311
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-08 17:01:23 +0000

    refactor: extracted repeated code update: added menu button functionality

--- src/components/Editor/Menu.tsx
+++ src/components/Editor/Menu.tsx
@@ -1,12 +1,52 @@
-import React from 'react'
+import { Editor as CoreEditor } from '@tiptap/core'
+import { EditorContentProps, EditorContentState } from '@tiptap/react'
 
-const Menu: React.FC = () => {
+import React, {
+  ButtonHTMLAttributes,
+  Children,
+  DetailedHTMLProps,
+  MouseEventHandler
+} from 'react'
+declare class Editor extends CoreEditor {
+  contentComponent: React.Component<
+    EditorContentProps,
+    EditorContentState
+  > | null
+}
+
+const MenuButton: React.FC<
+  DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > & { active?: boolean }
+> = (props) => {
+  const { children, active } = props
+  return (
+    <button
+      className={`w-8 h-8   hover:text-gray-800 flex items-center justify-center rounded-md hover:bg-gray-300 hover:ring-2 hover:ring-blue-400 ${
+        active ? 'bg-gray-300 text-gray-600' : 'bg-gray-200 text-gray-600'
+      }`}
+      {...props}
+    >
+      {children}
+    </button>
+  )
+}
+
+const Menu: React.FC<{ editor: Editor }> = ({ editor }) => {
   return (
     <div className="w-full rounded-lg bg-gray-100 p-4 flex space-x-2 mb-8">
-      <div className="h-8 flex items-center rounded-md bg-gray-300 px-2">
+      <div
+        onClick={() => {}}
+        className="h-8 flex items-center rounded-md bg-gray-300 px-2"
+      >
         Times Roman
       </div>
-      <button className="w-8 h-8 bg-gray-300 text-gray-700 hover:text-gray-900 flex items-center justify-center rounded-md hover:bg-gray-400">
+      {/* BOLD */}
+      <MenuButton
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        active={editor.isActive('bold')}
+      >
         <svg
           className="w-4 h-4"
           xmlns="http://www.w3.org/2000/svg"
@@ -22,8 +62,12 @@ const Menu: React.FC = () => {
           <path d="M6 4h8a4 4 0 0 1 4 4 4 4 0 0 1-4 4H6z"></path>
           <path d="M6 12h9a4 4 0 0 1 4 4 4 4 0 0 1-4 4H6z"></path>
         </svg>
-      </button>
-      <button className="w-8 h-8 bg-gray-300 text-gray-700 hover:text-gray-900 flex items-center justify-center rounded-md hover:bg-gray-400">
+      </MenuButton>
+      {/* UNDERLINE */}
+      <MenuButton
+        onClick={() => editor.chain().focus().toggleUnderline().run()}
+        active={editor.isActive('underline')}
+      >
         <svg
           className="w-4 h-4"
           xmlns="http://www.w3.org/2000/svg"
@@ -39,8 +83,12 @@ const Menu: React.FC = () => {
           <path d="M6 3v7a6 6 0 0 0 6 6 6 6 0 0 0 6-6V3"></path>
           <line x1="4" y1="21" x2="20" y2="21"></line>
         </svg>
-      </button>
-      <button className="w-8 h-8 bg-gray-300 text-gray-700 hover:text-gray-900 flex items-center justify-center rounded-md hover:bg-gray-400">
+      </MenuButton>
+      {/* ITALICS */}
+      <MenuButton
+        onClick={() => editor.chain().focus().toggleItalic().run()}
+        active={editor.isActive('italic')}
+      >
         <svg
           className="w-4 h-4"
           xmlns="http://www.w3.org/2000/svg"
@@ -55,8 +103,9 @@ const Menu: React.FC = () => {
         >
           <path d="M19 4h-9M14 20H5M14.7 4.7L9.2 19.4" />
         </svg>
-      </button>
-      <button className="w-8 h-8 bg-gray-300 text-gray-700 hover:text-gray-900 flex items-center justify-center rounded-md hover:bg-gray-400">
+      </MenuButton>
+      {/* TEXT TYPE */}
+      <MenuButton>
         <svg
           className="w-4 h-4"
           xmlns="http://www.w3.org/2000/svg"
@@ -73,8 +122,12 @@ const Menu: React.FC = () => {
           <line x1="9" y1="20" x2="15" y2="20"></line>
           <line x1="12" y1="4" x2="12" y2="20"></line>
         </svg>
-      </button>
-      <button className="w-8 h-8 bg-gray-300 text-gray-700 hover:text-gray-900 flex items-center justify-center rounded-md hover:bg-gray-400">
+      </MenuButton>
+      {/* LEFT ALIGN */}
+      <MenuButton
+        onClick={() => editor.chain().focus().setTextAlign('left').run()}
+        active={editor.isActive({ textAlign: 'left' })}
+      >
         <svg
           className="w-4 h-4"
           xmlns="http://www.w3.org/2000/svg"
@@ -89,8 +142,12 @@ const Menu: React.FC = () => {
         >
           <path d="M17 9.5H3M21 4.5H3M21 14.5H3M17 19.5H3" />
         </svg>
-      </button>
-      <button className="w-8 h-8 bg-gray-300 text-gray-700 hover:text-gray-900 flex items-center justify-center rounded-md hover:bg-gray-400">
+      </MenuButton>
+      {/* CENTER ALIGN */}
+      <MenuButton
+        onClick={() => editor.chain().focus().setTextAlign('center').run()}
+        active={editor.isActive({ textAlign: 'center' })}
+      >
         <svg
           className="w-4 h-4"
           xmlns="http://www.w3.org/2000/svg"
@@ -105,8 +162,12 @@ const Menu: React.FC = () => {
         >
           <path d="M19 9.5H5M21 4.5H3M21 14.5H3M19 19.5H5" />
         </svg>
-      </button>
-      <button className="w-8 h-8 bg-gray-300 text-gray-700 hover:text-gray-900 flex items-center justify-center rounded-md hover:bg-gray-400">
+      </MenuButton>
+      {/* RIGHT ALIGN */}
+      <MenuButton
+        onClick={() => editor.chain().focus().setTextAlign('right').run()}
+        active={editor.isActive({ textAlign: 'right' })}
+      >
         <svg
           className="w-4 h-4"
           xmlns="http://www.w3.org/2000/svg"
@@ -121,7 +182,7 @@ const Menu: React.FC = () => {
         >
           <path d="M21 9.5H7M21 4.5H3M21 14.5H3M21 19.5H7" />
         </svg>
-      </button>
+      </MenuButton>
     </div>
   )
 }
