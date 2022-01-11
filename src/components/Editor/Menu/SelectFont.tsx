8161ca7dc4da522e77aca79369912e1211817a1e
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-11 21:29:37 +0000

    refactor: removed bloated code fixed: fixed styling issues on select button

--- src/components/Editor/Menu/SelectFont.tsx
+++ src/components/Editor/Menu/SelectFont.tsx
@@ -1,56 +1,29 @@
 import React, { Fragment, useState } from 'react'
-import { useGoogleFonts, GoogleFontsStatus } from '@flyyer/use-googlefonts'
 import { Listbox, Transition } from '@headlessui/react'
 import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
 
-const fonts = [
-  {
-    name: 'Inter',
-    css: "'Inter', sans-serif",
-    styles: ['100..900', '100..900italic']
-  },
-  {
-    name: 'Roboto',
-    css: "'Roboto', sans-serif",
-    styles: ['100..900', '100..900italic']
-  },
-  {
-    name: 'Open Sans',
-    css: "''Open Sans', sans-serif",
-    styles: ['100..900', '100..900italic']
-  },
-  {
-    name: 'Poppins',
-    css: "'Poppins', sans-serif",
-    styles: ['100..900', '100..900italic']
-  },
-  {
-    name: 'Playfair Display',
-    css: "'Playfair Display', sans-serif",
-    styles: ['100..900', '100..900italic']
-  },
-  {
-    name: 'Roboto Mono',
-    css: "'Roboto Mono', sans-serif",
-    styles: ['100..900', '100..900italic']
-  },
-  {
-    name: 'Source Code Pro',
-    css: "'Source Code Pro', sans-serif",
-    styles: ['100..900', '100..900italic']
-  }
-]
+const fonts = ['Brush Script MT', 'Courier New', 'Inter', 'Times New Roman']
 
-const SelectFont: React.FC = () => {
+const SelectFont: React.FC<{ changeFont: (family: string) => void }> = ({
+  changeFont
+}) => {
   const [selected, setSelected] = useState(fonts[0])
 
+  const onSelectChange = (f: string) => {
+    setSelected(f)
+    changeFont(f)
+  }
+
   return (
-    <Listbox value={selected} onChange={setSelected}>
+    <Listbox value={selected} onChange={onSelectChange}>
       {({ open }) => (
         <>
           <div className="relative">
-            <Listbox.Button className="bg-gray-200 relative w-36 border h-8 rounded-md shadow-sm pl-3 pr-10 py-1.5 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-              <span className="block truncate">{selected.name}</span>
+            <Listbox.Button
+              className="relative bg-gray-200 w-48 h-8 rounded-md shadow-sm border pl-3 pr-10 py-1.5 text-left cursor-default sm:text-sm
+            focus:outline-none focus:ring-1 focus:ring-blue-400 focus:border-blue-400"
+            >
+              <span className="block truncate">{selected}</span>
               <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
                 <SelectorIcon
                   className="h-5 w-5 text-gray-400"
@@ -67,15 +40,15 @@ const SelectFont: React.FC = () => {
               leaveTo="opacity-0"
             >
               <Listbox.Options className="scrollbar absolute z-10 mt-1 w-full bg-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
-                {fonts.map(({ name }) => (
+                {fonts.map((font) => (
                   <Listbox.Option
-                    key={name}
+                    key={font}
                     className={({ active }) =>
                       `${
-                        active ? 'text-white bg-indigo-600' : 'text-gray-900'
+                        active ? 'text-white bg-blue-400' : 'text-gray-900'
                       } cursor-default select-none relative py-2 pl-3 pr-9`
                     }
-                    value={name}
+                    value={font}
                   >
                     {({ selected, active }) => (
                       <>
@@ -86,15 +59,15 @@ const SelectFont: React.FC = () => {
                             block truncate)
                           `}
                         >
-                          {name}
+                          {font}
                         </span>
 
                         {selected ? (
                           <span
                             className={`${
-                              active ? 'text-white' : 'text-indigo-600'
+                              active ? 'text-white' : 'text-blue-400'
                             } 
-                              absolute inset-y-0 right-0 flex items-center pr-4)
+                              absolute inset-y-0 right-0 flex items-center pr-4
                             }`}
                           >
                             <CheckIcon className="h-5 w-5" aria-hidden="true" />
