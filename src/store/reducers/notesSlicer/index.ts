38fb5aec2583b8036f557d72a2a8de8e350bf755
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-12 21:47:35 +0000

    update: added updateNoteAction

--- src/store/reducers/notesSlicer/index.ts
+++ src/store/reducers/notesSlicer/index.ts
@@ -1,4 +1,5 @@
 import { CaseReducer, createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { Content } from '@tiptap/core'
 
 import type { RootState } from '../../index'
 import initialState, { NotesState, Note } from './types'
@@ -10,15 +11,32 @@ const createNoteAction: CaseReducer<NotesState, PayloadAction<Note>> = (
   return { ...state, notes: [...state.notes, action.payload] }
 }
 
+const updateNoteAction: CaseReducer<NotesState, PayloadAction<Note>> = (
+  state,
+  action
+) => {
+  return {
+    ...state,
+    notes: state.notes.map((n) =>
+      n.id === action.payload.id ? action.payload : n
+    ),
+    openedNote: {
+      ...state.openedNote,
+      note: action.payload
+    }
+  }
+}
+
 export const NotesSlice = createSlice({
   name: 'notes',
   initialState,
   reducers: {
-    createNote: createNoteAction
+    createNote: createNoteAction,
+    updateNote: updateNoteAction
   }
 })
 
-export const { createNote } = NotesSlice.actions
+export const { createNote, updateNote } = NotesSlice.actions
 
 export const selectNote = (state: RootState) => state.present.notes
 
