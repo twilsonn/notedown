dcd103edd91dcb251708906438326febc7102da6
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-12 20:11:07 +0000

    update: loading opened note from state

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { useEditor, EditorContent } from '@tiptap/react'
+import { useEditor, EditorContent, generateHTML } from '@tiptap/react'
 
 import Typography from '@tiptap/extension-typography'
 import Placeholder from '@tiptap/extension-placeholder'
@@ -19,25 +19,28 @@ import Underline from '@tiptap/extension-underline'
 import TextAlign from '@tiptap/extension-text-align'
 import FontFamily from '@tiptap/extension-font-family'
 import TextStyle from '@tiptap/extension-text-style'
+import { useAppSelector } from 'hooks'
 
 const CustomDocument = Document.extend({
   content: 'heading block*'
 })
 
-export default () => {
+const TipTapEditor = () => {
+  const openedNote = useAppSelector((state) => state.present.openedNote)
+
   let editor = useEditor({
     extensions: [
       StarterKit.configure({
         document: false
       }),
       CustomDocument,
-      Paragraph,
+      // Paragraph,
       TextStyle,
       FontFamily.configure({
         types: ['textStyle']
       }),
-      Text,
-      Code,
+      // Text,
+      // Code,
       Typography,
       ColorHighlighter,
       SmilieReplacer,
@@ -50,28 +53,14 @@ export default () => {
         placeholder: 'Write something …'
       })
     ],
-    content: `
-      <h1>
-        It'll always have a heading …
-      </h1>
-      <p>
-        … if you pass a custom document. That's the beauty of having full control over the schema.
-      </p>
-      <p>
-        → With the Typography extension, tiptap understands »what you mean« and adds correct characters to your text — it's like a “typography nerd” on your side.
-      </p>
-      <p>
-        Try it out and type <code>(c)</code>, <code>-></code>, <code>>></code>, <code>1/2</code>, <code>!=</code>, <code>--</code> or <code>1x1</code> here:
-      </p>
-      <p></p>
-      <p>
-        Or add completely custom input rules. We added a custom extension here that replaces smilies like <code>:-)</code>, <code><3</code> or <code>>:P</code> with emojis. Try it out:
-      </p>
-      <p></p>
-      <p>
-        You can also teach the editor new things. For example to recognize hex colors and add a color swatch on the fly: #FFF, #0D0D0D, #616161, #A975FF, #FB5151, #FD9170, #FFCB6B, #68CEF8, #80cbc4, #9DEF8F
-      </p>
-    `
+    content: '',
+    onCreate: ({ editor }) => {
+      editor.commands.setContent(openedNote.note.content)
+    },
+    onUpdate: ({ editor }) => {
+      const json = editor.getJSON()
+      console.log(json)
+    }
   })
 
   return (
@@ -81,3 +70,5 @@ export default () => {
     </>
   )
 }
+
+export default TipTapEditor
