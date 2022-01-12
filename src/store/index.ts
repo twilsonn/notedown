1976ac525e734ea2a27319a7520df84ef2be9bf0
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-12 20:07:52 +0000

    added: store with persist

---
+++ src/store/index.ts
@@ -0,0 +1,26 @@
+import { createStore } from '@reduxjs/toolkit'
+
+import notesReducer from './reducers/notesSlicer'
+import undoable from 'redux-undo'
+
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key: 'todos',
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, undoable(notesReducer))
+
+const store = createStore(persistedReducer)
+
+const persistor = persistStore(store)
+
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
+
+export default () => {
+  return { store, persistor }
+}
