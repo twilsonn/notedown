764e3aa626ea48b5dec1aba9d71126e2314a7574
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-13 20:52:06 +0000

    refactor: extensions import from file update: added title update

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,30 +1,15 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import debounce from 'lodash.debounce'
-import humanizeDuration from 'humanize-duration'
 
 import { useEditor, EditorContent, Editor } from '@tiptap/react'
 
-import Typography from '@tiptap/extension-typography'
-import Placeholder from '@tiptap/extension-placeholder'
-import StarterKit from '@tiptap/starter-kit'
-import Document from '@tiptap/extension-document'
-
-import { ColorHighlighter } from './ColorHighligher'
-import { SmilieReplacer } from './SmileReplacer'
 import Menu from './Menu'
 
 import './styles.css'
-import Underline from '@tiptap/extension-underline'
-import TextAlign from '@tiptap/extension-text-align'
-import FontFamily from '@tiptap/extension-font-family'
-import TextStyle from '@tiptap/extension-text-style'
 import { useAppDispatch, useAppSelector } from 'hooks'
 import { updateNote } from 'store/reducers/notesSlicer'
 import LastSaved from './LastSaved'
-
-const CustomDocument = Document.extend({
-  content: 'heading block*'
-})
+import extensions from './extensions'
 
 const TipTapEditor = () => {
   const openedNote = useAppSelector((state) => state.present.openedNote)
@@ -34,7 +19,13 @@ const TipTapEditor = () => {
   const updateOpenedNote = (e: Editor) => {
     console.log('updated')
     const content = e.getJSON()
-    dispatch(updateNote({ id: openedNote.id, content }))
+    dispatch(
+      updateNote({
+        id: openedNote.id,
+        content,
+        title: e.getText().split('\n')[0]
+      })
+    )
 
     const newLastSaved = new Date(Date.now())
     setLastSaved(newLastSaved)
@@ -46,27 +37,7 @@ const TipTapEditor = () => {
   )
 
   let editor = useEditor({
-    extensions: [
-      StarterKit.configure({
-        document: false
-      }),
-      CustomDocument,
-      TextStyle,
-      FontFamily.configure({
-        types: ['textStyle']
-      }),
-      Typography,
-      ColorHighlighter,
-      SmilieReplacer,
-      Underline,
-      TextAlign.configure({
-        types: ['heading', 'paragraph']
-      }),
-      Placeholder.configure({
-        emptyEditorClass: 'is-editor-empty font',
-        placeholder: 'Write something …'
-      })
-    ],
+    extensions,
     onCreate: ({ editor }) => {
       editor.commands.setContent(openedNote.note.content)
     },
