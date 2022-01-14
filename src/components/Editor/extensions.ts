12f5604cae83c9b34734d88b3c6ffc71b30ea32a
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-14 22:10:36 +0000

    update: removed default starterKit update: added custom extensions list

--- src/components/Editor/extensions.ts
+++ src/components/Editor/extensions.ts
@@ -1,7 +1,8 @@
 import Document from '@tiptap/extension-document'
 import Typography from '@tiptap/extension-typography'
 import Placeholder from '@tiptap/extension-placeholder'
-import StarterKit from '@tiptap/starter-kit'
+import Paragraph from '@tiptap/extension-paragraph'
+import Text from '@tiptap/extension-text'
 
 import { ColorHighlighter } from './ColorHighligher'
 import { SmilieReplacer } from './SmileReplacer'
@@ -10,24 +11,46 @@ import TextAlign from '@tiptap/extension-text-align'
 import FontFamily from '@tiptap/extension-font-family'
 import TextStyle from '@tiptap/extension-text-style'
 import { Extensions } from '@tiptap/core'
+import History from '@tiptap/extension-history'
+import Italic from '@tiptap/extension-italic'
+import Strike from '@tiptap/extension-strike'
+import Bold from '@tiptap/extension-bold'
+import Gapcursor from '@tiptap/extension-gapcursor'
+import Heading from '@tiptap/extension-heading'
+import Blockquote from '@tiptap/extension-blockquote'
+import Dropcursor from '@tiptap/extension-dropcursor'
+import Code from '@tiptap/extension-code'
 
 const CustomDocument = Document.extend({
   content: 'heading block*'
 })
 
 const extensions: Extensions = [
-  StarterKit.configure({
-    document: false
-  }),
   CustomDocument,
+  History,
+
+  Text,
+  Heading,
+  Blockquote,
+  Paragraph,
+
+  Underline,
+  Italic,
+  Bold,
+  Strike,
+  Code,
+
+  Gapcursor,
+  Dropcursor,
+
   TextStyle,
+  Typography,
+
   FontFamily.configure({
     types: ['textStyle']
   }),
-  Typography,
-  ColorHighlighter,
   SmilieReplacer,
-  Underline,
+  ColorHighlighter,
   TextAlign.configure({
     types: ['heading', 'paragraph']
   }),
