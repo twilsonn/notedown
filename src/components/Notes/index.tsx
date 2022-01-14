dc0fa27ad5c2ae885ea17be2f66253cc785c13ea
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-14 20:23:25 +0000

    update: added functionality to new note button

--- src/components/Notes/index.tsx
+++ src/components/Notes/index.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { generateHTML } from '@tiptap/react'
 import extensions from 'components/Editor/extensions'
 
-import { useAppSelector } from 'hooks'
+import { useAppDispatch, useAppSelector } from 'hooks'
 import Logo from 'assets/Logo'
 import NewNote from './NewNote'
 import { PlusIcon } from '@heroicons/react/solid'
+import { newNote } from 'store/reducers/notesSlicer'
 
 const Note: React.FC<{ title: string; content: string }> = ({
   title,
@@ -13,7 +14,7 @@ const Note: React.FC<{ title: string; content: string }> = ({
 }) => {
   const descrpition = content.replace(/<[^>]*>?/gm, '')
   const descrpitionLength = 120
-  const titleLength = 30
+  const titleLength = 20
   return (
     <div className="prose px-3 py-2 bg-gray-200 rounded-lg">
       <div className="flex justify-between items-center">
@@ -24,7 +25,7 @@ const Note: React.FC<{ title: string; content: string }> = ({
         </h4>
         <p className="text-xs m-0">updated at 16:45</p>
       </div>
-      <p className="text-sm leading-tight">
+      <p className="text-sm leading-tight wrap break-all">
         {descrpition.length > descrpitionLength
           ? descrpition.substring(0, descrpitionLength - 3) + '...'
           : descrpition}
@@ -37,6 +38,8 @@ const Notes: React.FC = () => {
   const notes = useAppSelector((state) => state.present.notes)
   const openedNote = useAppSelector((state) => state.present.openedNote)
 
+  const dispatch = useAppDispatch()
+
   return (
     <>
       <header className="p-4 h-24 flex items-center">
@@ -47,6 +50,7 @@ const Notes: React.FC = () => {
         <div className="px-4 py-4 mb-1 flex justify-between items-center">
           <h2 className="font-semibold text-lg">Notes</h2>
           <button
+            onClick={() => dispatch(newNote())}
             className={`px-2 py-1 border flex items-center justify-center rounded-md group text-sm bg-white text-gray-600
               hover:ring-1 hover:border-blue-400 hover:ring-blue-400 hover:bg-gray-50 hover:text-gray-800
               focus:ring-1 focus:border-blue-400 focus:ring-blue-400 focus:outline-none`}
