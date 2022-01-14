e493db1828575b887dc2bd4c34f7408319c24a9d
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-14 20:23:49 +0000

    update: renamed store key

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,7 +7,7 @@ import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 
 const persistConfig = {
-  key: 'todos',
+  key: 'notes',
   storage
 }
 
