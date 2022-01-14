522ce25d30d1e58a4e1f981be95cefd0aefe7aa2
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-14 20:24:45 +0000

    update: added basic responsive layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,14 +5,12 @@ import Notes from './Notes'
 
 function App() {
   return (
-    <div className="m-auto">
-      <div className="flex relative">
-        <div className="min-h-screen max-w-sm w-full bg-gray-100">
-          <Notes />
-        </div>
-        <div className="w-full min-h-screen focus:outline-none px-12 flex flex-col relative">
-          <Tiptap />
-        </div>
+    <div className="m-auto flex">
+      <div className="lg:flex flex-col min-h-screen hidden lg:w-1/3 xl:w-1/4 2xl:w-1/5 bg-gray-100">
+        <Notes />
+      </div>
+      <div className="flex flex-col w-full lg:w-2/3 xl:w-3/4 2xl:w-4/5">
+        <Tiptap />
       </div>
     </div>
   )
