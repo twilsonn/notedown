6aee85f2e1863305c03af4fa21ed48d0c56609c9
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-15 13:39:21 +0000

    refactor: remvoed external library update: changed styling

--- src/components/Editor/LastSaved.tsx
+++ src/components/Editor/LastSaved.tsx
@@ -1,38 +1,51 @@
-// import humanizeDuration from 'humanize-duration'
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
+import { CheckCircleIcon, ExclamationCircleIcon } from '@heroicons/react/solid'
 
-// const shortEnglishHumanizer = humanizeDuration.humanizer({
-//   language: 'shortEn',
-//   languages: {
-//     shortEn: {
-//       y: () => 'y',
-//       mo: () => 'mo',
-//       w: () => 'w',
-//       d: () => 'd',
-//       h: () => 'h',
-//       m: () => 'm',
-//       s: () => 's',
-//       ms: () => 'ms'
-//     }
-//   }
-// })
-
-const LastSaved: React.FC<{ lastSaved: Date }> = ({ lastSaved }) => {
-  const [mil, setMil] = useState(500)
+const LastSaved: React.FC<{ lastSaved?: Date }> = ({ lastSaved }) => {
+  const [mil, setMil] = useState(lastSaved?.getMilliseconds())
 
   useEffect(() => {
+    setMil(0)
     let myInterval = setInterval(() => {
-      setMil(lastSaved.getTime() - Date.now())
-    }, 5000)
+      lastSaved ? setMil(Date.now() - lastSaved.getTime()) : null
+    }, 1000)
     return () => {
       clearInterval(myInterval)
     }
-  })
+  }, [lastSaved])
+
+  const formatTime = (t: number) => {
+    if (t >= 1000 * 60 * 60 * 24) {
+      return `${Math.floor(t / (1000 * 60 * 60 * 24))} h`
+    }
+
+    if (t >= 1000 * 60) {
+      return `${Math.floor(t / (1000 * 60))} m`
+    }
+
+    if (t < 1000 * 60) {
+      return `${Math.floor(t / 1000)} s`
+    }
+  }
 
   return (
-    <div className="fixed bottom-0 right-0 w-full bg-gray-300 px-4 py-2">
-      {/* Last Updated: {mil && shortEnglishHumanizer(mil, { round: true })} */}
+    <div className="fixed bottom-0 right-0 w-full bg-gray-300 px-4 h-8">
+      <div className="h-full w-full flex justify-end items-center text-xs text-gray-700 divide-x-2 divide-gray-400">
+        {lastSaved != undefined && mil != undefined && mil > 1000 ? (
+          <>
+            Last Updated:{' '}
+            <span className="pr-2 pl-1 text-right">{formatTime(mil)}</span>
+          </>
+        ) : null}
+
+        <div className="pl-2">
+          {lastSaved != undefined ? (
+            <CheckCircleIcon className="w-4 h-4 text-emerald-500" />
+          ) : (
+            <ExclamationCircleIcon className="w-4 h-4 text-amber-500" />
+          )}
+        </div>
+      </div>
     </div>
   )
 }
