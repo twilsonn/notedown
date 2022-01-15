71325bb19b1ea61dd022acc7b71df70422092519
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-15 13:39:42 +0000

    update: changed to fixed layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,10 +6,10 @@ import Notes from './Notes'
 function App() {
   return (
     <div className="m-auto flex">
-      <div className="lg:flex flex-col min-h-screen hidden lg:w-1/3 xl:w-1/4 2xl:w-1/5 bg-gray-100">
+      <div className="fixed lg:flex flex-col min-h-screen hidden lg:w-1/3 xl:w-1/4 2xl:w-1/5 bg-gray-100">
         <Notes />
       </div>
-      <div className="flex flex-col w-full lg:w-2/3 xl:w-3/4 2xl:w-4/5">
+      <div className="flex flex-col w-full lg:w-2/3 lg:ml-[33.333333%] xl:w-3/4 xl:ml-[25%] 2xl:w-4/5 2xl:ml-[20%]">
         <Tiptap />
       </div>
     </div>
