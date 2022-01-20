e743dcad03b0f7ef741c0823c78d85f4a6448bc1
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-20 21:41:59 +0000

    update: added darkmode to editior menu

--- src/components/Editor/Menu/SelectFont.tsx
+++ src/components/Editor/Menu/SelectFont.tsx
@@ -20,8 +20,8 @@ const SelectFont: React.FC<{ changeFont: (family: string) => void }> = ({
         <>
           <div className="relative">
             <Listbox.Button
-              className="relative bg-gray-200 w-48 h-8 rounded-md shadow-sm border pl-3 pr-10 py-1.5 text-left cursor-default sm:text-sm
-            focus:outline-none focus:ring-1 focus:ring-blue-400 focus:border-blue-400"
+              className="relative bg-gray-200 dark:bg-stone-800 dark:text-stone-200 dark:border-stone-800 w-48 h-8 rounded-md shadow-sm border pl-3 pr-10 py-1.5 text-left cursor-default sm:text-sm
+            focus:outline-none focus:ring-1 focus:ring-blue-400 focus:border-blue-400 dark:focus:ring-blue-700 dark:focus:border-blue-700"
             >
               <span className="block truncate">{selected}</span>
               <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
@@ -39,13 +39,15 @@ const SelectFont: React.FC<{ changeFont: (family: string) => void }> = ({
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <Listbox.Options className="scrollbar absolute z-10 mt-1 w-full bg-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+              <Listbox.Options className="scrollbar absolute z-10 mt-1 w-full bg-white dark:bg-stone-900 shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
                 {fonts.map((font) => (
                   <Listbox.Option
                     key={font}
                     className={({ active }) =>
                       `${
-                        active ? 'text-white bg-blue-400' : 'text-gray-900'
+                        active
+                          ? 'text-white bg-blue-400 dark:bg-stone-800'
+                          : 'text-gray-900 dark:text-stone-200'
                       } cursor-default select-none relative py-2 pl-3 pr-9`
                     }
                     value={font}
@@ -65,7 +67,9 @@ const SelectFont: React.FC<{ changeFont: (family: string) => void }> = ({
                         {selected ? (
                           <span
                             className={`${
-                              active ? 'text-white' : 'text-blue-400'
+                              active
+                                ? 'text-white dark:text-stone-600'
+                                : 'text-blue-400 dark:text-blue-700'
                             } 
                               absolute inset-y-0 right-0 flex items-center pr-4
                             }`}
