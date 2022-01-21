af5907f001584ed6259d26be3e32300acdf2b79d
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-21 21:08:26 +0000

    update: changed styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,7 +6,7 @@ import Notes from './Notes'
 function App() {
   return (
     <div className="m-auto flex">
-      <div className="fixed lg:flex flex-col min-h-screen hidden lg:w-1/3 xl:w-1/4 2xl:w-1/5 bg-gray-100 dark:bg-stone-900">
+      <div className="fixed lg:flex flex-col h-full max-h-screen overflow-hidden pb-8 hidden lg:w-1/3 xl:w-1/4 2xl:w-1/5 bg-gray-100 dark:bg-stone-900">
         <Notes />
       </div>
       <div className="flex flex-col w-full min-h-screen lg:w-2/3 lg:ml-[33.333333%] xl:w-3/4 xl:ml-[25%] 2xl:w-4/5 2xl:ml-[20%]">
