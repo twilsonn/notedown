47c7d8937ee279b6f49ad86b267931c3a42aa753
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-22 20:42:53 +0000

    update: fixed darkmode background update: added control bar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ControlBar from './ControlBar'
 
 import Tiptap from './Editor'
 import Notes from './Notes'
@@ -6,11 +7,12 @@ import Notes from './Notes'
 function App() {
   return (
     <div className="m-auto flex">
-      <div className="fixed lg:flex flex-col h-full max-h-screen overflow-hidden pb-8 hidden lg:w-1/3 xl:w-1/4 2xl:w-1/5 bg-gray-100 dark:bg-stone-900">
+      <div className="fixed lg:flex flex-col h-full max-h-screen overflow-hidden pb-8 hidden lg:w-1/3 xl:w-1/4 2xl:w-1/5 bg-gray-100 dark:bg-stone-900 transition-colors">
         <Notes />
       </div>
-      <div className="flex flex-col w-full min-h-screen lg:w-2/3 lg:ml-[33.333333%] xl:w-3/4 xl:ml-[25%] 2xl:w-4/5 2xl:ml-[20%]">
+      <div className="flex flex-col w-full min-h-screen lg:w-2/3 lg:ml-[33.333333%] xl:w-3/4 xl:ml-[25%] 2xl:w-4/5 2xl:ml-[20%] bg-white dark:bg-stone-800 transition-colors">
         <Tiptap />
+        <ControlBar />
       </div>
     </div>
   )
