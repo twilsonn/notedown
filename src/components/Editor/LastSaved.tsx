a0c2783ba371f1ff1c69c65253fe9818c44312b6
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-22 20:41:53 +0000

    update: modified for control bar

--- src/components/Editor/LastSaved.tsx
+++ src/components/Editor/LastSaved.tsx
@@ -19,38 +19,32 @@ const TimeNow: React.FC<React.HTMLAttributes<HTMLSpanElement>> = (props) => {
 const LastSaved: React.FC = () => {
   const openedNote = useAppSelector((state) => state.notes.present.openedNote)
 
-  return (
-    <div className="fixed bottom-0 right-0 w-full px-4 h-8 border-t bg-gray-300 border-gray-300 dark:bg-stone-900 dark:border-stone-800">
-      <div className="h-full w-full flex justify-end items-center text-xs divide-x-2  text-gray-700 divide-gray-400 dark:text-stone-300 dark:divide-stone-700">
-        {openedNote ? (
-          <>
-            {openedNote.note.saved ? (
-              <>
-                Last Updated:{' '}
-                <span className="pr-2 pl-1 text-right">
-                  <ReactTimeAgo
-                    date={openedNote.note.updatedAt}
-                    timeStyle="twitter"
-                  />
-                </span>
-              </>
-            ) : null}
-
-            <div className="pl-2">
-              {openedNote?.note.saved ? (
-                <CheckCircleIcon className="w-4 h-4 text-emerald-500" />
-              ) : (
-                <ExclamationCircleIcon className="w-4 h-4 text-amber-500" />
-              )}
-            </div>
-          </>
-        ) : (
+  return openedNote ? (
+    <div className="flex text-xs divide-x-2 text-gray-700 divide-gray-400 dark:text-stone-300 dark:divide-stone-700">
+      {openedNote.note.saved ? (
+        <>
+          Last Updated:{' '}
           <span className="pr-2 pl-1 text-right">
-            <TimeNow className="pr-2 pl-1 text-right" />
+            <ReactTimeAgo
+              date={openedNote.note.updatedAt}
+              timeStyle="twitter"
+            />
           </span>
+        </>
+      ) : null}
+
+      <div className="pl-2">
+        {openedNote?.note.saved ? (
+          <CheckCircleIcon className="w-4 h-4 text-emerald-500" />
+        ) : (
+          <ExclamationCircleIcon className="w-4 h-4 text-amber-500" />
         )}
       </div>
     </div>
+  ) : (
+    <span className="pr-2 pl-1 text-right">
+      <TimeNow className="pr-2 pl-1 text-right" />
+    </span>
   )
 }
 
