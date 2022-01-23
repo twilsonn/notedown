9c5986dc38338c9b941fab532d101ed78b15fd5a
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-23 22:14:35 +0000

    update: added content menu wrapper around notes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ContextMenuWrapper from './ContextMenuWrapper'
 import ControlBar from './ControlBar'
 
 import Tiptap from './Editor'
@@ -7,9 +8,11 @@ import Notes from './Notes'
 function App() {
   return (
     <div className="m-auto flex">
-      <div className="fixed lg:flex flex-col h-full max-h-screen overflow-hidden pb-8 hidden lg:w-1/3 xl:w-1/4 2xl:w-1/5 bg-gray-100 dark:bg-stone-900 transition-colors">
-        <Notes />
-      </div>
+      <ContextMenuWrapper>
+        <div className="fixed lg:flex flex-col h-full max-h-screen overflow-hidden pb-8 hidden lg:w-1/3 xl:w-1/4 2xl:w-1/5 bg-gray-100 dark:bg-stone-900 transition-colors">
+          <Notes />
+        </div>
+      </ContextMenuWrapper>
       <div className="flex flex-col w-full min-h-screen lg:w-2/3 lg:ml-[33.333333%] xl:w-3/4 xl:ml-[25%] 2xl:w-4/5 2xl:ml-[20%] bg-white dark:bg-stone-800 transition-colors">
         <Tiptap />
         <ControlBar />
