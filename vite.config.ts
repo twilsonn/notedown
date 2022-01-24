5375c87b5403dff66e82556bf3f734839bddb64a
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-24 19:28:17 +0000

    update: changed deployment to vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/notedown/',
+  base: '/',
   plugins: [react(), tsconfigPaths()]
 })
