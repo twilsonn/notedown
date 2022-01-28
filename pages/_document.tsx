df61865f044b981142e386ede890ce6c2fbdc844
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-28 14:19:37 +0000

    update: added head imports

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,31 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
-        <Head />
+      <Html className="bg-white dark:bg-stone-800">
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#3393f0" />
+          <meta name="msapplication-TileColor" content="#2d89ef" />
+          <meta name="theme-color" content="#ffffff" />
+          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        </Head>
         <body>
           <Main />
           <NextScript />
