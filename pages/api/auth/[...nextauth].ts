ca1534f553f3efe35580619d99e033e62bfb3ea6
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-31 19:20:44 +0000

    added: next-auth api

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,16 @@
+import NextAuth from 'next-auth'
+// import AppleProvider from 'next-auth/providers/apple'
+// import FacebookProvider from 'next-auth/providers/facebook'
+// import GoogleProvider from 'next-auth/providers/google'
+import GithubProvider from 'next-auth/providers/github'
+
+export default NextAuth({
+  providers: [
+    // OAuth authentication providers...
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET
+    })
+  ],
+  secret: process.env.APP_SECRET
+})
