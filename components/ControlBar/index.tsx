1a0cbdb72c04b83e7c2371324143fe29080f4846
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-31 19:21:17 +0000

    update: added oauth test

--- components/ControlBar/index.tsx
+++ components/ControlBar/index.tsx
@@ -1,12 +1,19 @@
+import { useSession } from 'next-auth/react'
 import React from 'react'
 import DarkModeToggle from '../DarkModeToggle'
 import LastSaved from '../Editor/LastSaved'
 
 const ControlBar: React.FC = () => {
+  const { data: session } = useSession()
   return (
     <div className="fixed bottom-0 right-0 w-full px-4 h-8 border-t bg-gray-300 border-gray-300 dark:bg-stone-900 dark:border-stone-800 transition-colors">
       <div className="h-full flex justify-between items-center">
-        <DarkModeToggle />
+        <div className="flex divide-x-2">
+          <p className="pr-3 prose-sm dark:text-stone-100">
+            logged in as: {session?.user?.name}
+          </p>
+          <DarkModeToggle />
+        </div>
         <LastSaved />
       </div>
     </div>
