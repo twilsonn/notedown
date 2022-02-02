96d9308cf6d1c053b496bd41d5d0b34c6485b4cc
Tyler Wilson <tylerwilson0729@outlook.com>
2022-02-02 23:18:02 +0000

    update: added settings

--- components/ControlBar/index.tsx
+++ components/ControlBar/index.tsx
@@ -1,18 +1,20 @@
-import { useSession } from 'next-auth/react'
 import React from 'react'
-import DarkModeToggle from '../DarkModeToggle'
+import { useSession } from 'next-auth/react'
 import LastSaved from '../Editor/LastSaved'
+import Settings from './Settings'
 
 const ControlBar: React.FC = () => {
   const { data: session } = useSession()
+
+  // const [showModal, hideModal] = useModal(() => {
+  //   return <Modal closeModal={hideModal}>test</Modal>
+  // })
+
   return (
     <div className="fixed bottom-0 right-0 w-full px-4 h-8 border-t bg-gray-300 border-gray-300 dark:bg-stone-900 dark:border-stone-800 transition-colors">
       <div className="h-full flex justify-between items-center">
-        <div className="flex divide-x-2">
-          <p className="pr-3 prose-sm dark:text-stone-100">
-            logged in as: {session?.user?.name}
-          </p>
-          <DarkModeToggle />
+        <div className="flex space-x-2 items-center">
+          <Settings />
         </div>
         <LastSaved />
       </div>
