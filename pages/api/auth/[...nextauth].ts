681fab899020af43d73f0b55264ddcdcf58416f2
Tyler Wilson <tylerwilson0729@outlook.com>
2022-02-04 22:36:46 +0000

    update: added DynamoDB adapter

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,28 @@
 import NextAuth from 'next-auth'
+import { DynamoDB, DynamoDBClientConfig } from '@aws-sdk/client-dynamodb'
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb'
+import { DynamoDBAdapter } from '@next-auth/dynamodb-adapter'
+
 // import AppleProvider from 'next-auth/providers/apple'
 // import FacebookProvider from 'next-auth/providers/facebook'
 // import GoogleProvider from 'next-auth/providers/google'
 import GithubProvider from 'next-auth/providers/github'
+
+const config: DynamoDBClientConfig = {
+  credentials: {
+    accessKeyId: process.env.NEXT_AUTH_AWS_ACCESS_KEY as string,
+    secretAccessKey: process.env.NEXT_AUTH_AWS_SECRET_KEY as string
+  },
+  region: process.env.NEXT_AUTH_AWS_REGION
+}
+
+const client = DynamoDBDocument.from(new DynamoDB(config), {
+  marshallOptions: {
+    convertEmptyValues: true,
+    removeUndefinedValues: true,
+    convertClassInstanceToMap: true
+  }
+})
 
 export default NextAuth({
   providers: [
@@ -12,5 +32,25 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET
     })
   ],
-  secret: process.env.APP_SECRET
+  secret: process.env.APP_SECRET,
+  adapter: DynamoDBAdapter(client, {
+    tableName: process.env.NOTEDOWN_DB_NAME
+  }),
+  callbacks: {
+    session: async ({ session, token }) => {
+      if (session?.user) {
+        session.user.id = token.uid
+      }
+      return session
+    },
+    jwt: async ({ user, token }) => {
+      if (user) {
+        token.uid = user.id
+      }
+      return token
+    }
+  },
+  session: {
+    strategy: 'jwt'
+  }
 })
