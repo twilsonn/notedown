d59fcd2cb14ee17bb059594aee8f39b02957d683
Tyler Wilson <tylerwilson0729@outlook.com>
2022-02-04 22:37:38 +0000

    added: sync api w/ DynamoDB

--- pages/api/sync.ts
+++ pages/api/sync.ts
@@ -1,17 +1,106 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import NextCors from 'nextjs-cors'
+import { getSession } from 'next-auth/react'
+import { DynamoDB, DynamoDBClientConfig } from '@aws-sdk/client-dynamodb'
+import {
+  DynamoDBDocument,
+  GetCommandInput,
+  PutCommandInput
+} from '@aws-sdk/lib-dynamodb'
+import dayjs from 'dayjs'
+
+const config: DynamoDBClientConfig = {
+  credentials: {
+    accessKeyId: process.env.NEXT_AUTH_AWS_ACCESS_KEY as string,
+    secretAccessKey: process.env.NEXT_AUTH_AWS_SECRET_KEY as string
+  },
+  region: process.env.NEXT_AUTH_AWS_REGION
+}
+
+const client = DynamoDBDocument.from(new DynamoDB(config), {
+  marshallOptions: {
+    convertEmptyValues: true,
+    removeUndefinedValues: true,
+    convertClassInstanceToMap: true
+  }
+})
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   await NextCors(req, res, {
-    // Options
-    methods: ['GET', 'POST', 'OPTIONS'],
+    methods: ['POST'],
     origin: '*',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    optionsSuccessStatus: 200
   })
 
-  // Rest of the API logic
-  res.json({ message: 'Hello World!' })
+  const session = await getSession({ req })
+  const body = JSON.parse(req.body)
+
+  if (session && session.user && session.user.id) {
+    let get: GetCommandInput = {
+      TableName: process.env.NOTEDOWN_DB_NAME,
+      Key: {
+        pk: `USER#${session.user.id}`,
+        sk: `NOTES#${session.user.id}`
+      }
+    }
+
+    try {
+      const data = await client.get(get)
+
+      if (data.Item) {
+        console.log(data.Item.ls, body.lastSync)
+
+        if (data.Item.ls >= body.lastSync) {
+          return res.json({
+            success: true,
+            notes: data.Item.notes,
+            lastSync: data.Item.ls
+          })
+        }
+      }
+    } catch (err) {
+      console.error(err)
+
+      res.statusCode = 500
+      return res.json({ error: true })
+    }
+
+    // =====================================
+
+    console.log('Last Sync from body', body.lastSync)
+
+    let put: PutCommandInput = {
+      TableName: process.env.NOTEDOWN_DB_NAME,
+      Item: {
+        pk: `USER#${session.user.id}`,
+        sk: `NOTES#${session.user.id}`,
+        notes: JSON.stringify(body.notes),
+        type: 'NOTES',
+        ls: body.lastSync,
+        expires: dayjs(new Date()).add(60, 'day').unix()
+      }
+    }
+
+    try {
+      const data = await client.put(put)
+      if (data.$metadata.httpStatusCode !== 200) {
+        throw new Error('Internal error')
+      }
+      return res.json({
+        success: true,
+        notes: body.notes,
+        lastSync: body.lastSync
+      })
+    } catch (err) {
+      console.error(err)
+
+      res.statusCode = 500
+      return res.json({ error: true })
+    }
+  }
+
+  return res.status(500)
 }
