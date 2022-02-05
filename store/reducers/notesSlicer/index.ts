0356f219d83a8831de1d2fcc82cbc66ab8bf2ae5
Tyler Wilson <tylerwilson0729@outlook.com>
2022-02-05 17:55:45 +0000

    update: added async syncNotes action

--- store/reducers/notesSlicer/index.ts
+++ store/reducers/notesSlicer/index.ts
@@ -1,4 +1,11 @@
-import { CaseReducer, createSlice, PayloadAction } from '@reduxjs/toolkit'
+import {
+  AnyAction,
+  CaseReducer,
+  createAsyncThunk,
+  createSlice,
+  PayloadAction,
+  ThunkAction
+} from '@reduxjs/toolkit'
 import { v4 as uuid } from 'uuid'
 
 import initialState, { NotesState, Note } from './types'
@@ -43,17 +50,29 @@ const updateNoteAction: CaseReducer<NotesState, PayloadAction<Note>> = (
   state,
   action
 ) => {
-  return {
-    ...state,
-    notes: state.notes.map((n) =>
-      n.id === action.payload.id ? action.payload : n
-    ),
-    openedNote: {
-      ...state.openedNote,
-      id: action.payload.id,
-      note: action.payload
+  let openedNoteIndex: number | undefined
+
+  const newNotes = state.notes.map((n, i) => {
+    if (n.id === action.payload.id) {
+      openedNoteIndex = i
+      return {
+        ...action.payload,
+        saved: true
+      }
     }
-  }
+    return n
+  })
+
+  return openedNoteIndex !== undefined
+    ? {
+        ...state,
+        notes: newNotes,
+        openedNote: {
+          id: action.payload.id,
+          note: newNotes[openedNoteIndex]
+        }
+      }
+    : state
 }
 
 const openNoteAction: CaseReducer<NotesState, PayloadAction<string>> = (
@@ -96,33 +115,31 @@ const removeNoteAction: CaseReducer<NotesState, PayloadAction<string>> = (
   }
 }
 
-const syncNotesAction: CaseReducer<
-  NotesState,
-  PayloadAction<{
-    notes: string
-    lastSync: number
-  }>
-> = (state, action) => {
-  return {
-    ...state,
-    syncing: false,
-    lastSync: action.payload.lastSync,
-    notes: JSON.parse(action.payload.notes)
-  }
-}
+export const syncNotes = createAsyncThunk<Note[], Note[]>(
+  'notes/syncNotes',
+  async (notes, api) => {
+    try {
+      const { success, data, error } = await fetch('api/sync', {
+        method: 'post',
+        body: JSON.stringify({
+          notes
+        })
+      }).then((res) => res.json())
 
-const startSyncAction: CaseReducer<NotesState> = (state) => {
-  return {
-    ...state,
-    syncing: true
+      if (success) {
+        console.log('Notes are up to date.')
+        return data.notes
+      } else if (success === false && error.code === 202) {
+        console.log('Notes have conflicts. Please resolve them')
+        throw new Error()
+      } else {
+        throw new Error(data.error.message)
+      }
+    } catch (error) {
+      throw new Error()
+    }
   }
-}
-const cancelSyncNotesAction: CaseReducer<NotesState> = (state) => {
-  return {
-    ...state,
-    syncing: false
-  }
-}
+)
 
 export const NotesSlice = createSlice({
   name: 'notes',
@@ -132,22 +149,30 @@ export const NotesSlice = createSlice({
     updateNote: updateNoteAction,
     openNote: openNoteAction,
     removeNote: removeNoteAction,
-    toggleNoteSaved: toggleNoteSavedAction,
-    syncNotes: syncNotesAction,
-    startSync: startSyncAction,
-    cancelSyncNotes: cancelSyncNotesAction
+    toggleNoteSaved: toggleNoteSavedAction
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(syncNotes.pending, (state) => {
+        state.syncing = true
+      })
+      .addCase(syncNotes.fulfilled, (state, action) => {
+        state.syncing = false
+        state.notes = action.payload
+        if (state.openedNote) {
+          state.openedNote = {
+            ...state.openedNote,
+            note: action.payload.filter((n) => n.id === state.openedNote?.id)[0]
+          }
+        }
+      })
+      .addCase(syncNotes.rejected, (state) => {
+        state.syncing = true
+      })
   }
 })
 
-export const {
-  newNote,
-  updateNote,
-  openNote,
-  removeNote,
-  toggleNoteSaved,
-  syncNotes,
-  startSync,
-  cancelSyncNotes
-} = NotesSlice.actions
+export const { newNote, updateNote, openNote, removeNote, toggleNoteSaved } =
+  NotesSlice.actions
 
 export default NotesSlice
