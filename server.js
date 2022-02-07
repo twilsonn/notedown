1c2e8aa720469516fb352534e829a2f5b1a8150d
Tyler Wilson <tylerwilson0729@outlook.com>
2022-02-07 20:36:19 +0000

    update: added variable address

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const next = require('next')
 const rateLimit = require('express-rate-limit')
 
 const port = parseInt(process.env.PORT, 10) || 3000
+const address = process.env.ADDRESS || 'localhost'
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
@@ -25,6 +26,6 @@ app.prepare().then(() => {
 
   server.listen(port, (err) => {
     if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
+    console.log(`> Ready on http://${address}:${port}`)
   })
 })
