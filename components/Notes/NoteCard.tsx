f16939711efd0c1222e028d0271607829ae7467d
Tyler Wilson <tylerwilson0729@outlook.com>
2022-02-12 21:49:59 +0000

    update: refactored styles

--- components/Notes/NoteCard.tsx
+++ components/Notes/NoteCard.tsx
@@ -28,28 +28,20 @@ const NoteCard: React.FC<
     <button
       data-note
       data-id={note.id}
-      className={`w-full text-left prose dark:prose-invert px-3 py-2 rounded-lg cursor-pointer select-none group
-        hover:bg-gray-300 dark:hover:bg-stone-700
-        focus:outline-none  focus:ring-2 focus:ring-blue-400 transition-colors ${
-          open === true
-            ? 'bg-gray-300 dark:bg-stone-700 text-black dark:text-stone-100'
-            : 'bg-gray-200 dark:bg-stone-800 dark:text-stone-200'
-        }`}
+      className={`note-card prose dark:prose-invert ${
+        open === true ? 'active' : ''
+      }`}
       {...props}
     >
       <div className="flex justify-between items-center pointer-events-none">
-        <h4
-          className={`m-0 capitalize group-hover:text-black dark:group-hover:text-stone-50`}
-        >
+        <h4 className="m-0 capitalize">
           {!title ? 'Untitled Note' : noteTitle}
         </h4>
         <p className="text-xs m-0">
           <TimeAgo date={new Date(updatedAt)} />
         </p>
       </div>
-      <p
-        className={`pointer-events-none text-sm leading-tight wrap break-all group-hover:text-black dark:group-hover:text-stone-50`}
-      >
+      <p className="pointer-events-none text-sm leading-tight wrap break-all">
         {!content
           ? '...'
           : content.length > contentLength
