8ba4d47f13d2f1cdbab769aad6683e6895ea0eb5
Tyler Wilson <tylerwilson0729@outlook.com>
2022-02-21 20:33:06 +0000

    update: renamed AppState to RootState

--- store/index.ts
+++ store/index.ts
@@ -59,18 +59,18 @@ const makeStore = () => {
 }
 
 export type AppStore = ReturnType<typeof makeStore>
-export type AppState = ReturnType<AppStore['getState']>
+export type RootState = ReturnType<AppStore['getState']>
 export type AppDispatch = AppStore['dispatch']
 
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
-  AppState,
+  RootState,
   unknown,
   Action
 >
 
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 
-export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 
 export const wrapper = createWrapper<AppStore>(makeStore)
