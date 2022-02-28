f8b30a99b5445882af2fe4d3d2d21fbf9d9ce6a5
Tyler Wilson <tylerwilson0729@outlook.com>
2022-02-28 21:59:05 +0000

    update: mobile works as intended

--- components/App.tsx
+++ components/App.tsx
@@ -5,20 +5,14 @@ import { useSession } from 'next-auth/react'
 import { useAppDispatch, useAppSelector } from '../store'
 import { syncNotes } from '../store/reducers/notesSlicer'
 
-import {
-  LayoutGroup,
-  motion,
-  MotionConfig,
-  useCycle,
-  Variants
-} from 'framer-motion'
+import { LayoutGroup, motion, MotionConfig, Variants } from 'framer-motion'
 import { useMediaQuery } from 'beautiful-react-hooks'
 
 import ContextMenuWrapper from './ContextMenuWrapper'
 import ControlBar from './ControlBar'
 import Notes from './Notes'
 import SyncConflict from './SyncConflict'
-import { toggleNavBar } from '../store/reducers/appReducer'
+import { setNavOpened, toggleNavBar } from '../store/reducers/appReducer'
 
 const LazyEditor = dynamic(() => import('./Editor'), {
   ssr: false
@@ -43,14 +37,14 @@ function App() {
   const is2Xl = useMediaQuery('(min-width: 1536px)')
 
   useEffect(() => {
-    if (!isLg && openedNote === undefined) {
-      dispatch(toggleNavBar(true))
-    } else {
-      dispatch(toggleNavBar(false))
+    if (!isLg) {
+      if (openedNote === undefined) {
+        dispatch(toggleNavBar(true))
+      } else {
+        dispatch(toggleNavBar(false))
+      }
     }
   }, [dispatch, isLg, openedNote])
-
-  console.log(open)
 
   const getSize = useCallback(
     (isSidebar: boolean) => {
@@ -75,14 +69,14 @@ function App() {
     () =>
       !isLg
         ? {
-            open: { translateX: '100%', marginLeft: 0 },
-            closed: { translateX: '0%', marginLeft: 0 }
+            open: { translateX: '100%', marginLeft: 0, width: '100%' },
+            closed: { translateX: '0%', marginLeft: 0, width: '100%' }
           }
         : {
             open: {
               translateX: '0%',
-              marginLeft: getSize(false),
-              width: '100%'
+              width: '100%',
+              marginLeft: getSize(false)
             },
             closed: { translateX: '0%', marginLeft: 0, width: '100%' }
           },
@@ -94,7 +88,7 @@ function App() {
       !isLg
         ? {
             open: { translateX: '0%', width: '100%' },
-            closed: { translateX: '-100%' }
+            closed: { translateX: '-100%', width: '100%' }
           }
         : {
             open: { translateX: 0, width: getSize(true) },
@@ -114,35 +108,47 @@ function App() {
   }, [EditorVariants, SidebarVariants])
 
   return (
-    <div className="m-auto flex overflow-hidden w-screen h-screen">
-      <MotionConfig
-        transition={{ duration: 0.2, ease: 'linear', type: 'tween' }}
-      >
+    <div className="flex overflow-hidden">
+      {/* <LazyMotion features={domAnimation}> */}
+      <MotionConfig transition={{ duration: 0.2 }}>
         <LayoutGroup>
-          <ContextMenuWrapper key="aside">
+          <ContextMenuWrapper>
             <motion.aside
               layout
               initial={false}
               animate={open ? 'open' : 'closed'}
               variants={sidebarVariants}
-              className="fixed lg:flex flex-col h-full max-h-screen overflow-hidden pb-8 lg:w-1/3 xl:w-1/4 2xl:w-1/5 bg-gray-100 dark:bg-stone-900 transition-colors z-50"
+              onAnimationComplete={() => {
+                if (!isLg) {
+                  if (open) {
+                    dispatch(setNavOpened(true))
+                  } else {
+                    dispatch(setNavOpened(false))
+                  }
+                }
+              }}
+              className="fixed lg:flex flex-col h-full overflow-hidden pb-8 lg:w-1/3 xl:w-1/4 2xl:w-1/5 bg-gray-100 dark:bg-stone-900 transition-colors z-50"
             >
               <Notes />
             </motion.aside>
           </ContextMenuWrapper>
 
           <motion.div
-            key="editor"
             layout
             initial={false}
             animate={open ? 'open' : 'closed'}
             variants={editorVariants}
-            className="flex flex-col w-full min-h-screen lg:w-2/3 lg:ml-[33.333333%] xl:w-3/4 xl:ml-[25%] 2xl:w-4/5 2xl:ml-[20%] bg-white dark:bg-stone-800 transition-colors"
+            style={{
+              WebkitOverflowScrolling: 'touch',
+              overflowY: 'auto'
+            }}
+            className="flex flex-col w-full min-h-full m-0 lg:w-2/3 lg:ml-[33.333333%] xl:w-3/4 xl:ml-[25%] 2xl:w-4/5 2xl:ml-[20%] bg-white dark:bg-stone-800 transition-colors"
           >
             {lastSync === null && syncing ? null : <LazyEditor />}
           </motion.div>
         </LayoutGroup>
       </MotionConfig>
+      {/* </LazyMotion> */}
 
       <SyncConflict />
       <ControlBar />
